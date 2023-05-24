2e7681d0ff532289aa036f29c721841f3adfe8da
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-05-24 12:34:42 +0200

    Summary

--- assets/js/burgermenu.js
+++ assets/js/burgermenu.js
@@ -1,6 +1,44 @@
-const burgerIcon = document.getElementById('burgerIcon');
-const nav = document.querySelector('nav');
+// Hent DOM-elementer
+const burgerIcon = document.getElementById('burgerIcon'); // Burger-ikonet
+const menuList = document.getElementById('menuList'); // Menu-listen
+const nav = document.querySelector('nav'); // <nav> elementet
+const container = document.getElementById('container'); // Container-elementet
+const actTableCell = document.getElementsByClassName('act-table-cell'); // Elementer med klassen 'act-table-cell'
+const menuItems = menuList.querySelectorAll('li'); // Henter alle LI elementer inde i menuList
 
+// BURGER ICON ANIMATION
+const bgm1 = document.getElementById('bgm-1');
+const bgm2 = document.getElementById('bgm-2');
+const bgm3 = document.getElementById('bgm-3');
+
+// Lyt efter klik begivenhed på burger-ikonet
 burgerIcon.addEventListener('click', () => {
-    nav.classList.toggle('active')
+    menuList.classList.toggle('active');
+    nav.classList.toggle('active');
+    container.classList.toggle('active');
+    bgm1.classList.toggle('active');
+    bgm2.classList.toggle('active');
+    bgm3.classList.toggle('active');
+
+    // Loop gennem alle elementer med klassen 'act-table-cell'
+    for (let i = 0; i < actTableCell.length; i++) {
+        actTableCell[i].classList.toggle('active');
+    }
 });
+
+// Loop gennem alle menu-items og tilføj klik-begivenhed
+for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+        menuList.classList.remove('active');
+        nav.classList.remove('active');
+        container.classList.remove('active');
+        bgm1.classList.remove('active');
+        bgm2.classList.remove('active');
+        bgm3.classList.remove('active');
+
+        // Loop gennem alle elementer med klassen 'act-table-cell'
+        for (let i = 0; i < actTableCell.length; i++) {
+            actTableCell[i].classList.remove('active');
+        }
+    });
+}
--- assets/js/rotating_Site.js
+++ assets/js/rotating_Site.js
@@ -1,41 +1,68 @@
-(function(document, window, $) {
-	$(document).ready(function() {
-	  // Variables
-	  var windowWidth = $(window).width(),
-		  windowHeight = $(window).height(),
-		  $header = $('header'),
-		  reverseDirection; // Declare reverseDirection variable
-  
-	  // header anchor tags
-	  function headerAnchors() {
-		var pageDirection = '';
-		var thisElement;
-		var timeout;
-		$header.find('nav li a').click(function(event) {
-		  event.preventDefault();
-		  $('.cube').removeClass('reverse-' + pageDirection);
-		  thisElement = $(this);
-		  pageDirection = thisElement.data('direction');
-		  reverseDirection = thisElement.data('reverse-direction');
-		  thisElement.parent().addClass('active').siblings().removeClass('active');
-		  $('.cube').addClass('reverse-' + pageDirection);
-  
-		  $header.addClass('go-out');
-		  $('#wrap').addClass('active');
-		  clearTimeout(timeout);
-		  timeout = setTimeout(function() {
-			$header.removeClass('go-out');
-			$('#wrap').removeClass('active');
-		  }, 1000);
+(function (document, window, $) {
+	$(document).ready(function () {
+		// Variables
+		var windowWidth = $(window).width(),
+			windowHeight = $(window).height(),
+			$header = $('header'),
+			reverseDirection; // Declare reverseDirection variable
+
+		// header anchor tags
+		function headerAnchors() {
+			var pageDirection = ''; // Initialize pageDirection variable
+			var thisElement; // Declare thisElement variable
+			var timeout; // Declare timeout variable
+
+			// Event handler for $header.find('nav li a')
+			$header.find('nav li a').click(function (event) {
+				event.preventDefault();
+				$('.cube').removeClass('reverse-' + pageDirection);
+				thisElement = $(this);
+				pageDirection = thisElement.data('direction');
+				reverseDirection = thisElement.data('reverse-direction');
+				thisElement.parent().addClass('active').siblings().removeClass('active');
+				$('.cube').addClass('reverse-' + pageDirection);
+
+				$header.addClass('go-out');
+				$('#wrap').addClass('active');
+				clearTimeout(timeout);
+				timeout = setTimeout(function () {
+					$header.removeClass('go-out');
+					$('#wrap').removeClass('active');
+				}, 1000);
+			});
+
+			// Event handler for #menuList li a
+			$('#menuList li a').click(function (event) {
+				event.preventDefault();
+				var targetDirection = $(this).data('direction');
+				var $headerNavItem = $header.find('nav li a[data-direction="' + targetDirection + '"]');
+
+				$('.cube').removeClass('reverse-' + pageDirection);
+				thisElement = $(this);
+				pageDirection = thisElement.data('direction');
+				reverseDirection = thisElement.data('reverse-direction');
+				thisElement.parent().addClass('active').siblings().removeClass('active');
+				$('.cube').addClass('reverse-' + pageDirection);
+
+				$header.addClass('go-out');
+				$('#wrap').addClass('active');
+				clearTimeout(timeout);
+				timeout = setTimeout(function () {
+					$header.removeClass('go-out');
+					$('#wrap').removeClass('active');
+				}, 1000);
+
+				$headerNavItem.parent().addClass('active').siblings().removeClass('active');
+			});
+		}
+
+		headerAnchors();
+
+		$(window).resize(function () {
+			// Vars
+			windowWidth = $(window).width();
+			windowHeight = $(window).height();
+			// Functions
 		});
-	  }
-	  headerAnchors();
-	  $(window).resize(function() {
-		// Vars
-		windowWidth = $(window).width();
-		windowHeight = $(window).height();
-		// Functions
-	  });
 	});
-  })(document, window, jQuery);
-  
+})(document, window, jQuery);
